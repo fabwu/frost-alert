3d0c44dd57569a0d4cfd9b741fc6d3df6a0c72d8
Fabian Wüthrich <me@fabwu.ch>
2020-05-03 20:49:13 +0200

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ return [
     'frost_threshold_deg' => 4.0,
     
     // https://support.google.com/maps/answer/18539?co=GENIE.Platform%3DDesktop&hl=en#
-	'latitude' =>  0.000000,
+    'latitude' =>  0.000000,
     'longitude' => 0.000000,
     
     // https://developer.srgssr.ch/content/quickstart-guide
